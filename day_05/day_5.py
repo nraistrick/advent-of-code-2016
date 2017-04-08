90a8288d0a3411024f38549f78034a7f91a0dc2b
Nicholas Raistrick <nraistrick123@gmail.com>
2017-04-08 11:11:33 +0100

    Day 5: Added advanced password calculation for part 2

--- day_05/day_5.py
+++ day_05/day_5.py
@@ -7,6 +7,7 @@ from common.common import get_md5_hash
 
 DOOR_INPUT = "wtnhxymk"
 PASSWORD_LENGTH = 8
+HEXADECIMAL_BASE = 16
 
 
 def calculate_password(door_id):
@@ -30,9 +31,38 @@ def calculate_password(door_id):
     return password
 
 
+def calculate_advanced_password(door_id):
+    """
+    Calculates the password for the more advanced algorithm
+
+    :type door_id: str
+    :rtype: str
+    """
+    password = ""
+    password_store = [""] * PASSWORD_LENGTH
+
+    for i in itertools.count():
+        value_to_hash = "%s%s" % (door_id, str(i))
+        hash_value = get_md5_hash(value_to_hash)
+        if hash_value.startswith("00000"):
+            character_position = int(hash_value[5], HEXADECIMAL_BASE)
+            if character_position >= PASSWORD_LENGTH:
+                continue
+            else:
+                character = hash_value[6]
+                if password_store[character_position] == "":
+                    password_store[character_position] = character
+                    password = "".join(password_store)
+                    if len(password) == PASSWORD_LENGTH:
+                        break
+
+    return password
+
+
 def main():
-    return calculate_password(DOOR_INPUT)
+    print "The simple password is: %s" % calculate_password(DOOR_INPUT)
+    print "The advanced password is %s" % calculate_advanced_password(DOOR_INPUT)
 
 
 if __name__ == "__main__":
-    print main()
+    main()
--- day_05/test_password_calculator.py
+++ day_05/test_password_calculator.py
@@ -1,14 +1,19 @@
 import unittest
-from day_05.day_5 import calculate_password
+from day_05.day_5 import calculate_password, calculate_advanced_password
 
 
 class TestPasswordCalculator(unittest.TestCase):
     door_input = "abc"
     valid_password = "18f47a30"
+    valid_advanced_password = "05ace8e3"
 
-    def test_password_is_correct(self):
+    def test_password_correct(self):
         password = calculate_password(TestPasswordCalculator.door_input)
         self.assertEqual(TestPasswordCalculator.valid_password, password)
+
+    def test_advanced_password_correct(self):
+        password = calculate_advanced_password(TestPasswordCalculator.door_input)
+        self.assertEqual(TestPasswordCalculator.valid_advanced_password, password)
 
 
 if __name__ == '__main__':
