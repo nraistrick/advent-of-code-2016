b4ff14d7aea72218cfcfcebb7dc04347b9692103
Nicholas Raistrick <nraistrick123@gmail.com>
2017-04-08 20:14:02 +0100

    Day 5: Pulled out common logic to improve code re-use and clarity

--- day_05/day_5.py
+++ day_05/day_5.py
@@ -10,62 +10,74 @@ PASSWORD_LENGTH = 8
 HEXADECIMAL_BASE = 16
 
 
-def calculate_password(door_id):
+def password_hash_values(door_id):
     """
+    Generates a list of valid hashes for password values
+
     :type door_id: str
+    :return: A hash representing the door ID appended with an incrementing integer
     :rtype: str
     """
-    password = ""
+    password_hash_identifier = "00000"
 
     for i in itertools.count():
         value_to_hash = "%s%s" % (door_id, str(i))
         hash_value = get_md5_hash(value_to_hash)
-        if not hash_value.startswith("00000"):
-            # Not a hash value of interest
-            continue
 
-        password += hash_value[5]
-        if len(password) == PASSWORD_LENGTH:
+        if hash_value.startswith(password_hash_identifier):
+            # Not a hash value of interest
+            yield hash_value
+
+
+def calculate_password(door_id):
+    """
+    Calculate the basic password
+
+    :param door_id: The door ID
+    :return: The password
+    :rtype: str
+    """
+    index_of_password_character = 5
+
+    password_store = [""] * PASSWORD_LENGTH
+    for hash_value in password_hash_values(door_id):
+        character = hash_value[index_of_password_character]
+        password_store[password_store.index("")] = character
+
+        if "" not in password_store:
+            # If we have all the characters
             break
 
-    return password
+    # There should be only characters in the password store
+    return "".join(password_store)
 
 
 def calculate_advanced_password(door_id):
     """
-    Calculates the password for the more advanced algorithm
+    Calculate the advanced password
 
-    :type door_id: str
+    :param door_id: The door ID
+    :return: The password
     :rtype: str
     """
-    password = ""
     password_store = [""] * PASSWORD_LENGTH
-
-    for i in itertools.count():
-        value_to_hash = "%s%s" % (door_id, str(i))
-        hash_value = get_md5_hash(value_to_hash)
-
-        if not hash_value.startswith("00000"):
-            # Not a hash value of interest
-            continue
-
+    for hash_value in password_hash_values(door_id):
         character_position = int(hash_value[5], HEXADECIMAL_BASE)
         if character_position >= PASSWORD_LENGTH:
             # Character position doesn't fit within password length limits
             continue
 
-        if password_store[character_position] != "":
-            # We've already stored a character at this position -
-            # we only want the first one.
+        if password_store[character_position]:
+            # We only use the first character found for each position
             continue
 
         password_store[character_position] = hash_value[6]
-        password = "".join(password_store)
-        if len(password) == PASSWORD_LENGTH:
-            # We've successfully got all the password characters
+        if "" not in password_store:
+            # If we have all the characters
             break
 
-    return password
+    # There should be only characters in the password store
+    return "".join(password_store)
 
 
 def main():
--- day_05/test_password_calculator.py
+++ day_05/test_password_calculator.py
@@ -1,11 +1,21 @@
 import unittest
-from day_05.day_5 import calculate_password, calculate_advanced_password
+from day_05.day_5 import password_hash_values, \
+    calculate_password, \
+    calculate_advanced_password
 
 
 class TestPasswordCalculator(unittest.TestCase):
     door_input = "abc"
     valid_password = "18f47a30"
     valid_advanced_password = "05ace8e3"
+
+    def test_password_hash_values(self):
+        hash_values = password_hash_values("wtnhxymk")
+        first_hash_values = [next(hash_values) for _ in range(3)]
+        self.assertEqual(first_hash_values,
+                         ['0000027b9705c7e6fa3d4816c490bbfd',
+                          '00000468c8625d85571d250737c47b5a',
+                          '0000013e3293b49e4c78a5b43b21023b'])
 
     def test_password_correct(self):
         password = calculate_password(TestPasswordCalculator.door_input)
