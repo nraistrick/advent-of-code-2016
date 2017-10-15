e2c00ab3070258a29b85fec3963f2fe384edb232
Nicholas Raistrick <nraistrick123@gmail.com>
2017-10-15 14:38:11 +0100

    Day 12: Initialised register 'c' to 1 to solve part 2

--- day_12/main.py
+++ day_12/main.py
@@ -11,6 +11,14 @@ def main():
 
     print "Final registry values: %s " % str(runner.registry)
 
+    print "Running assembunny instructions with register 'c' initialised to 1..."
+    registry = AssemblyRunner.create_registry()
+    registry['c'] = 1
+    runner.registry = registry
+    runner.execute()
+
+    print "Final registry values: %s " % str(runner.registry)
+
 
 if __name__ == '__main__':
     main()
